9c5a1d735e30b9e92db430d3cae88fd44d94dc3a
DaniinXorchenabo <rkbcu@mail.ru>
2021-12-21 00:01:27 +0300

    получение и отправка данных на `raspberry` работают.

--- src/main.cpp
+++ src/main.cpp
@@ -11,18 +11,31 @@
 #include "DHT.h"
 
 
-
-void setup() {
-    // put your setup code here, to run once:
-    pinMode(13, OUTPUT);
-    Serial.begin(115200);
-    Serial.println("Could not find a valid BMP085 sensor, check wiring!");
-}
-
-void loop() {
-    digitalWrite(13, HIGH);
-    delay(500);
-    digitalWrite(13, LOW);
-    delay(500);
-    Serial.println("--");
-}
+//
+//void setup() {
+//    // put your setup code here, to run once:
+//    pinMode(13, OUTPUT);
+//    Serial.begin(9600);
+//    Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+//}
+//
+//void _loop() {
+//    digitalWrite(13, HIGH);
+//    delay(500);
+//    digitalWrite(13, LOW);
+//    delay(500);
+//    Serial.println("--");
+//}
+//
+//void loop(){
+//
+//    Serial.print("-");
+//
+//    if (Serial.available()) {
+//        String data = "";
+//        Serial.setTimeout(50);
+//        data = Serial.readString();
+//        Serial.println("rpi_get: " + data);
+//    }
+//    delay(100);
+//}
