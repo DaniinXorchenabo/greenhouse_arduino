0f97a254c935a148048fed76134260fd9b6125f0
DaniinXorchenabo <rkbcu@mail.ru>
2021-12-22 12:35:01 +0300

    add `json` parser

--- src/tests/test_serial_port.cpp
+++ src/tests/test_serial_port.cpp
@@ -9,6 +9,8 @@
 #include <iterator>
 #include <map>
 #include "DHT.h"
+//#include <json/value.h>
+#include <ArduinoJson.h>
 
 unsigned long time_turn_on;
 
@@ -35,21 +37,66 @@ void loop() {
         String data = "";
         Serial.setTimeout(50);
         data = Serial.readString();
-        Serial.println("rpi_get: " + data);
+
+        DynamicJsonDocument doc(1024);
+        deserializeJson(doc, data);
+        if (!doc.isNull()) {
+            if (!doc["set"].isNull()) {
+                if (!doc["set"]["led"].isNull()) {
+                    serializeJson(doc["set"]["led"], Serial);
+                    Serial.print("\n");
+                }
+                if (!doc["set"]["reb_blue_led"].isNull()) {
+                    serializeJson(doc["set"]["reb_blue_led"], Serial);;
+                    Serial.print("\n");
+                }
+                if (!doc["set"]["humidity"].isNull()) {
+                    serializeJson(doc["set"]["humidity"], Serial);;
+                    Serial.print("\n");
+                }
+                if (!doc["set"]["root_humidity"].isNull()) {
+                    serializeJson(doc["set"]["root_humidity"], Serial);;
+                    Serial.print("\n");
+                }
+                if (!doc["set"]["temperature"].isNull()) {
+                    serializeJson(doc["set"]["temperature"], Serial);;
+                    Serial.print("\n");
+                }
+                if (!doc["set"]["fan"].isNull()) {
+                    serializeJson(doc["set"]["fan"], Serial);;
+                    Serial.print("\n");
+                }
+                if (!doc["set"]["root_fan"].isNull()) {
+                    serializeJson(doc["set"]["root_fan"], Serial);;
+                    Serial.print("\n");
+                }
+            }
+            if (doc["get"].isNull()) {
+
+            }
+            if (doc["action"].isNull()) {
+                doc["action"]["water"];
+            }
+            Serial.println("-----");
+            serializeJson(doc, Serial);
+            Serial.println("\ninto if");
+        }
+        Serial.print("rpi_get: " + data + "||");
+        Serial.println((!doc.isNull()));
     }
     delay(100);
 
     if (millis() - time_turn_on > 2000) {
         time_turn_on = millis();
         String data = String("") +
-               "{\"metrics\": {" +
-               "\"temperature\":" + (String) rand() + ", " +
-               "\"humidity\": " + (String) rand() + ", " +
-               "\"root_humidity\": " + (String) rand() + ", " +
-               "\"water_level\": " + (String) rand() + ", " +
-               "\"water_humidity_level\": " + (String) rand() +
-               "}"
-               "}";
+                      "{\"metrics\": {" +
+                      "\"temperature\":" + (String) rand() + ", " +
+                      "\"humidity\": " + (String) rand() + ", " +
+                      "\"root_humidity\": " + (String) rand() + ", " +
+                      "\"water_level\": " + (String) rand() + ", " +
+                      "\"water_humidity_level\": " + (String) rand() +
+                      "}"
+                      "}";
         Serial.println(data);
     }
 }
